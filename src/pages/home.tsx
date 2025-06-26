cebb976f591f206a53be85f0607b1ca146cf3f15
Dabaghi-Anass <anass.debbaghi123@gmail.com>
2025-06-26 22:00:50 +0100

    added caftans page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,40 +1,23 @@
-import { useEffect, useRef } from 'react';
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { ThreeGLTF3DModel } from '../components/model-scene';
+
 export function HomePage() {
-  function getModelPath(modelName: string): string {
-    return 'src/assets/models/' + modelName;
-  }
+  // function getModelPath(modelName: string): string {
+  //   return 'src/assets/models/' + modelName;
+  // }
   return (
-    <div>
-      <ThreeGLTF3DModel
-        width={window.innerHeight * 0.6}
-        height={window.innerHeight * 0.6}
-        modelPath={getModelPath('blue_candle_on_moroccan_plate.glb')}
+    <div className="landing">
+      {/* <ThreeGLTF3DModel
+        width={window.innerHeight}
+        height={window.innerHeight}
+        modelPath={getModelPath('ziyana.glb')}
         whiteLightIntensity={500}
         yellowLightIntensity={1500}
-        zoom={10}
+        zoom={1.8}
         style={{
-          border: '1px solid black',
+          border: "1px solid red"
         }}
-        onModelLoaded={(model) => {
-          model.parent?.position.set(0, 1, 0);
-        }}
-      />
-      {/* <ThreeGLTF3DModel
-        width={100}
-        height={100}
-        modelPath={getModelPath('berrad.glb')}
-        enableControls={true}
-        whiteLightIntensity={5000}
-        yellowLightIntensity={500}
-        zoom={10}
-        onModelLoaded={(model) => {
-          model.parent?.position.set(0, 0.3, 0);
-        }}
+
       /> */}
+      <img src="" />
     </div>
   );
 }
