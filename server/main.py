ecaad64454f7e2e56a2ef60f42137dbc8109ffb4
Dabaghi-Anass <anass.debbaghi123@gmail.com>
2025-06-27 12:29:12 +0100

    port change

--- server/main.py
+++ server/main.py
@@ -595,7 +595,7 @@ async def health_check():
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 8000))
+    port = int(os.getenv("PORT", 8080))
     host = os.getenv("HOST", "0.0.0.0")
     debug = os.getenv("DEBUG", "False").lower() == "true"
     
